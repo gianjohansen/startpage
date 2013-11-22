b47e1bdb84782f831aec996a6c8dec1699274a25
gianjohansen <gian.johansen@gmail.com>
2013-11-22 11:19:21 +1100

    Initial commit!

---
+++ js/functions.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+	// initialize clock
+    showTime();
+
+    // source: http://simpleweatherjs.com
+    $.simpleWeather({
+        zipcode: '',
+        // this is the id for sydney, lookup your city at http://woeid.rosselliot.co.nz/lookup/
+        woeid: '55864169',
+        location: '',
+        unit: 'f',
+        success: function (weather) {
+        	// build html
+            html = '<h1>Today</h1>';
+            html += '<ul><li class="currently">' + weather.currently + '</li>';
+            html += '<li>' + weather.highAlt + '&deg;C</li></ul>';
+            html += '<h1 id="tomorrow">Tomorrow</h1>';
+            html += '<ul><li class="currently">' + weather.tomorrow.forecast + '</li>';
+            html += '<li>' + weather.tomorrow.highAlt + '&deg;C</li></ul>';
+
+            $("#weather").html(html);
+            $(".centered").fadeIn(400);
+        },
+        error: function (error) {
+            $("#weather").html('<p>' + error + '</p>');
+        }
+    });
+});
+
+// set and update time
+function showTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    h = checkTime(h);
+    m = checkTime(m);
+    $("#clock").text(h + ":" + m);
+    t = setTimeout('showTime()', 1000);
+}
+// add a leading 0 to our digits
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
